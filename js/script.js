9b048df5914a6f8970f057b4dbba771e48749e1e
PietroFiori91 <pederflowers@hotmail.it>
2023-12-07 01:22:33 +0100

    reload-animation-start-GSAP

--- js/script.js
+++ js/script.js
@@ -17,3 +17,21 @@ window.addEventListener("mousemove", (e) => {
     }px)) translateY(calc(-50% + ${yValue * speedy}px))`;
   });
 });
+
+// GSAP
+// GSAP
+// GSAP
+// GSAP
+
+let timeline = gsap.timeline();
+
+parallax_el.forEach((el) => {
+  timeline.from(
+    el,
+    {
+      top: `${el.offsetHeight / 2 + el.dataset.distance}px`,
+      duration: 1,
+    },
+    "1"
+  );
+});
